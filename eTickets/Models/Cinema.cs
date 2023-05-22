d491dc9bf1a2602e897be4ae9837d1949bac9b29
Nour Solaiman <solaiman.nour@gmail.com>
2023-05-22 19:18:00 +0200

    Adding all views is done

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -6,9 +6,18 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Cinema logo")]
         public string Logo { get; set; }
+
+        [Display(Name = "Cinema Name")]
+
         public string Name { get; set; }
+
+        [Display(Name = "Description")]
+
         public string Description { get; set; }
+
         //Relationships
         public List<Movie> Movies { get; set; }
     }
