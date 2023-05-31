770339b08fdf29ea6b25cdbbea7f9d5edb3ca9ce
BojanMirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2023-05-31 23:10:30 +0200

    Admin Role Added in to App

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -2,12 +2,13 @@ using eTickets.Data;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 namespace eTickets
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -19,13 +20,18 @@ namespace eTickets
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
 
             //Identity function configuration
-            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
-            
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+          
             //Service configure,Registring Service
             builder.Services.AddScoped<IActorsService, ActorsService>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+           
 
             var app = builder.Build();
 
@@ -55,8 +61,46 @@ namespace eTickets
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            // Seed database
+            // Seed app database
             AppDbInitializer.Seed(app);
+
+            // Seeding Admin/User info
+            using(var scope=app.Services.CreateScope()) 
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var roles = new[] { "Admin", "Manager", "User" };
+                 foreach (var role in roles) 
+                 {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                 }
+            }
+
+            using(var scope = app.Services.CreateScope()) 
+            {  
+                var userManager=scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                
+               //seting default value for admin account 
+                string email = "BojanAdmin";
+                string password = "Bojan1981!";
+
+                if (await userManager.FindByEmailAsync(email)==null)
+                {
+                    var user = new IdentityUser();
+                    user.UserName = email;
+                    user.Email = email;
+
+                    //Creating Admin User
+                    await userManager.CreateAsync(user, password);
+
+                    //Put the User to his Admin Role
+                    await userManager.AddToRoleAsync(user, "Admin");
+
+                }
+            }
+
 
             app.Run();
         }
